209e8a61afad62e1e65073bf950b11d0ef7fbde3
Benj Fassbind <randombenj@gmail.com>
2016-04-20 08:47:19 +0200

    add bootstrap alert error messages

--- web/app/components/detail/detail.controller.js
+++ web/app/components/detail/detail.controller.js
@@ -14,7 +14,7 @@
    * @param {[type]} $http
    *  Module to make async HTTP Requests
    */
-  function DetailController ( $scope, $http, $routeParams ) {
+  function DetailController ( $scope, $rootScope, $http, $routeParams ) {
 
     if ( $routeParams.hasOwnProperty( 'package' ) ) {
 
@@ -95,10 +95,22 @@
           // display the search results
           $scope.package = data.data.package;
 
-          if ( $scope.package.versions.length > 0 ) {
+          if ( $scope.package && $scope.package.versions.length > 0 ) {
 
             // display the first version
             loadVersionData( $scope.package.versions[0].version );
+          } else {
+
+            // the package was not found ...
+            // create empty alerts if they are not existent
+            if ( !$rootScope.alerts ) $rootScope.alerts = [];
+
+            // add alert
+            $rootScope.alerts.push({
+              type: 'warning',
+              msg: 'Requested Package: "' + packageName + '" was not found on the Server.'
+            });
+
           }
         },
 
